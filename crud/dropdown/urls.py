4eee640f03010c509cbba80d0842a5afbd2c4b20
Adam Earle <robotlightsyou@gmail.com>
2020-09-19 22:38:32 -0700

    dependent dropdown working
    City dropdown is now dependent on country dropdown being selected. Still having issues loading into python shell, but since I can load everything through the admin panel I don't think this will be an issue long term. Guide at https://simpleisbetterthancomplex.com/tutorial/2018/01/29/how-to-implement-dependent-or-chained-dropdown-list-with-django.html
    
    additional resources:
    https://www.srihash.org/ #generate hash for new version of linked frameworks(nb jquery)
    https://stackoverflow.com/questions/51254596/how-to-get-the-integrity-value-for-a-jquery-version-for-script #slim version of jquery doesn't include ajax
    https://stackoverflow.com/questions/18271251/typeerror-ajax-is-not-a-function

--- crud/dropdown/urls.py
+++ crud/dropdown/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
-from. import views
+from . import views
 
 urlpatterns = [
     path('', views.PersonListView.as_view(), name='person_changelist'),
     path('add/', views.PersonCreateView.as_view(), name='person_add'),
     path('<int:pk>/', views.PersonUpdateView.as_view(), name='person_change'),
+
+    path('ajax/load-cities/', views.load_cities, name='ajax_load_cities'),
 ]
--- crud/dropdown/views.py
+++ crud/dropdown/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
-from .models import Person
+from .models import Person, City
+from .forms import PersonForm
 
 
 class PersonListView(ListView):
@@ -11,11 +12,19 @@ class PersonListView(ListView):
 
 class PersonCreateView(CreateView):
     model = Person
-    fields = ('name', 'birthdate', 'country', 'city')
+    form_class = PersonForm
+    # fields = ('name', 'birthdate', 'country', 'city')
     success_url = reverse_lazy('person_changelist')
 
 
 class PersonUpdateView(UpdateView):
     model = Person
-    fields = ('name', 'birthdate', 'country', 'city')
+    form_class = PersonForm
+    # fields = ('name', 'birthdate', 'country', 'city')
     success_url = reverse_lazy('person_changelist')
+
+
+def load_cities(request):
+    country_id = request.GET.get('country')
+    cities = City.objects.filter(country_id=country_id).order_by('name')
+    return render(request, 'dropdown/cities_dropdown.html', {'cities': cities})
